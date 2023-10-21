be492d172d762aca9261c022808fe2f2cb64d909
HaikalFajar03 <haikalfajar51@gmail.com>
2023-10-21 16:28:53 +0700

    Tugas 2 Minggu 5 (A. Muh. Haikal Fajar)

--- models/user.php
+++ models/user.php
@@ -5,9 +5,10 @@ class User {
     private $id;
     private $username;
 
-    public function __construct($id, $username) {
+    public function __construct($id, $username, $photo) {
         $this->id = $id;
         $this->username = $username;
+        $this->photo = $photo;
     }
 
     public function getId() {
